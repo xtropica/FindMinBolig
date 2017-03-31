12fa2e13630f8b6d4587a9ddd5848971e071e07f
MAJ <mc-johansen@hotmail.com>
2017-03-31 12:11:50 +0200

    Så er der selve fondamentet klar til de store ændringer

---
+++ find_min_bolig/find_min_bolig/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(find_min_bolig.Startup))]
+namespace find_min_bolig
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
